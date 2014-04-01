c00ebd7a400ba51b78d04935e9f3005954fedf12
Tomasz Piekarec <tomasz.piekarec@vtssoftware.com>
2014-04-01 12:39:36 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeoClient::Application.config.secret_token = '155da487f6a2ab9efdd688344d4b3ac64c878c43532a6ffaac108773a01a5acab59bc0011da9094850ce48693c94bf926188c52f458aa05ac7bd51b8e7c2ca57'
